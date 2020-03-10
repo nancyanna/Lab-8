705da09326cdd4853446f630839f9e2f4e6c866f
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-03-10 11:58:56 -0500

    Add files via upload

---
+++ RealityTV/Class4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealityTV
+{
+    class ParadiseIsland : Application
+    {
+        public string gender;
+        public string nameOfBoyOrGirlFriend;
+        public int yearsDating;
+
+        public ParadiseIsland(string gender, string nameOfBoyOrGirlFriend, int yearsDating,
+                        string firstName, string lastName, string dateOfBirth, string address, string city, string state, int zip,
+                                    string phoneNumber, string email) : base(firstName, lastName, dateOfBirth, address, city, state, zip, phoneNumber, email)
+        {
+            this.gender = gender;
+            this.nameOfBoyOrGirlFriend = nameOfBoyOrGirlFriend;
+            this.yearsDating = yearsDating;
+        }
+
+        public override void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("You've been accepted to Paradise Island");
+        }
+    }
+}
